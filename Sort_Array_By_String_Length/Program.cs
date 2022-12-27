4e6c7c5be88ef2a4c5f0a2e8a2253604e88aa724
Дима Ларионов <dima_larionov_000@mail.ru>
2022-12-27 16:01:35 +0300

    Добавьте файлы проекта.

---
+++ Sort_Array_By_String_Length/Program.cs
@@ -0,0 +1,7 @@
+﻿using Sort_Array_By_String_Length;
+
+var result=SortArrayByStringLength.SortByLength(new string[] {"asffsa","sada","ads","gdfgdgdgfgf"});
+foreach(var item in result)
+{
+    Console.WriteLine(item);
+}
---
+++ Sort_Array_By_String_Length/SortArrayByStringLength.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort_Array_By_String_Length;
+
+public class SortArrayByStringLength
+{
+    public static string[] SortByLength(string[] array)
+    {
+        string temp;
+        for (int i = 0; i < array.Length; i++)
+        {
+            for (int j = i+1; j < array.Length; j++)
+            {
+                if (array[i].Length > array[j].Length)
+                {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        return array;
+    }
+}
